7c9fae7114f5dfed392baa71624a2021d5e62ece
Stephen Baidu <stephenbaidu@gmail.com>
2019-03-25 15:05:11 +0000

    Setup shopify_api gem

---
+++ config/initializers/shopify.rb
@@ -0,0 +1,6 @@
+api_key = Rails.application.credentials.shopify_api_key
+password = Rails.application.credentials.shopify_password
+# shop_name = Rails.application.credentials.shopify_shop_name
+shop_name = 'stephen-baidu-dev'
+shop_url = "https://#{api_key}:#{password}@#{shop_name}.myshopify.com/admin"
+ShopifyAPI::Base.site = shop_url
