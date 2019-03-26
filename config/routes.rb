6a200023647755f7d51f1f2517d28ff12f96251c
Stephen Baidu <stephenbaidu@gmail.com>
2019-03-26 14:58:19 +0000

    Enable sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  require 'sidekiq/web'
+  require 'sidekiq/cron/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   root 'home#index'
   get '/profit_evolution', to: 'home#profit_evolution'
 
